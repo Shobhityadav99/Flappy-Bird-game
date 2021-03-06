8d0445c71a3aced3b0068b1b6308368490b0b907
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-06 19:53:04 +0530

    Fixing Blit

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import sys
 import pygame
 from pygame.locals import *
 
-FPS=32
-SCREENWIDTH=289
-SCREENHEIGHT=511
-SCREEN=pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+FPS = 32
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 GROUNDY = SCREENHEIGHT * 0.8
-GAME_SPRITES={}
-GAME_SOUNDS={}
-PLAYER='gallery/sprites/bird.png'
-BACKGROUND='gallery/sprites/background.png'
-PIPE='gallery/sprites/pipe.png'
+GAME_SPRITES = {}
+GAME_SOUNDS = {}
+PLAYER = 'gallery/sprites/bird.png'
+BACKGROUND = 'gallery/sprites/background.png'
+PIPE = 'gallery/sprites/pipe.png'
 
 def welcomeScreen():
     playerx = int(SCREENWIDTH/5)
-    playery = int((SCREENHEIGHT - GAME_SPRITES['player'].get_height())/2)
-    messagex = int((SCREENWIDTH - GAME_SPRITES['message'].get_width())/2)
+    # playery = int((SCREENHEIGHT - GAME_SPRITES['player'].get_height())/2)
+    playery = int(SCREENHEIGHT/2)
+    # messagex = int((SCREENWIDTH - GAME_SPRITES['message'].get_width())/2)
+    messagex = int((SCREENWIDTH)/2)
     messagey = int(SCREENHEIGHT*0.13)
     basex=0
     while True:
@@ -25,15 +27,13 @@ def welcomeScreen():
             if event.type == QUIT or (event.type == KEYDOWN and event.type == K_ESCAPE):
                 pygame.quit()
                 sys.exit()
-
             elif event.type == KEYDOWN and (event.type==K_SPACE or event.type == K_UP):
                 return
-
             else:
-                SCREEN.blit(GAME_SPRITES['background'],(0,0))
-                SCREEN.blit(GAME_SPRITES['player'],(playerx,playery))
-                SCREEN.blit(GAME_SPRITES['message'],(messagex,messagey))
-                SCREEN.blit(GAME_SPRITES['base'],(basex,GROUNDY))
+                SCREEN.blit(GAME_SPRITES['background'], (0, 0))    
+                SCREEN.blit(GAME_SPRITES['player'], (playerx, playery))    
+                SCREEN.blit(GAME_SPRITES['message'], (messagex,messagey ))    
+                SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))    
                 pygame.display.update()
                 FPSCLOCK.tick(FPS)
 
@@ -69,9 +69,9 @@ if __name__ == '__main__':
     GAME_SOUNDS['swoosh']=pygame.mixer.Sound('gallery/audio/swoosh.wav')
     GAME_SOUNDS['wing']=pygame.mixer.Sound('gallery/audio/wing.wav')
 
-    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert(),
-    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha(),
+    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert()
+    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
 
     while True:
         welcomeScreen()
-        mainGame()
+        # mainGame()
