2044ad58b389905f3db85dce09c1c8395bd279db
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-06 15:46:05 +0530

    Global variables defined

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import sys
 import pygame
 from pygame.locals import *
 
+FPS=32
+SCREENWIDTH=289
+SCREENHEIGHT=511
+SCREEN=pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+GROUNDY = SCREENHEIGHT * 0.8
+GAME_SPRITES={}
+GAME_SOUNDS={}
+PLAYER='/gallery/sprites/bird.png'
+BACKGROUND='/gallery/sprites/background.png'
+PIPE='/gallery/sprites/pipe.png'
+
