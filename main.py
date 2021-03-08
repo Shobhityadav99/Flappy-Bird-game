3c57438e279a853f8bfcfaa6b812ea2d3c7affab
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-08 18:55:21 +0530

    Pipes position configured

--- main.py
+++ main.py
@@ -14,37 +14,50 @@ PLAYER = 'gallery/sprites/bird.png'
 BACKGROUND = 'gallery/sprites/background.png'
 PIPE = 'gallery/sprites/pipe.png'
 
+
 def welcomeScreen():
     playerx = int(SCREENWIDTH/5)
     playery = int((SCREENHEIGHT - GAME_SPRITES['player'].get_height())/2)
     messagex = int((SCREENWIDTH - GAME_SPRITES['message'].get_width())/2)
     messagey = int(SCREENHEIGHT * 0.13)
-    basex=0
+    basex = 0
     while True:
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYDOWN and (event.key==K_SPACE or event.key == K_UP):
+            elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
                 return
             else:
                 SCREEN.blit(GAME_SPRITES['background'], (0, 0))
                 SCREEN.blit(GAME_SPRITES['player'], (playerx, playery))
-                SCREEN.blit(GAME_SPRITES['message'], (messagex,messagey))
-                SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))    
+                SCREEN.blit(GAME_SPRITES['message'], (messagex, messagey))
+                SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))
                 pygame.display.update()
                 FPSCLOCK.tick(FPS)
 
+
 def mainGame():
-    score=0
-    playerx=int(SCREENWIDTH /5)
-    playery=int(SCREENHEIGHT /2)
-    basex=0
-    newpipe1=getRandomPipe()
-    newpipe2=getRandomPipe()
+    score = 0
+    playerx = int(SCREENWIDTH / 5)
+    playery = int(SCREENHEIGHT / 2)
+    basex = 0
+    newpipe1 = getRandomPipe()
+    newpipe2 = getRandomPipe()
+
 
 def getRandomPipe():
-    pass
+    pipeHight = GAME_SPRITES['pipe'][0].get_height()
+    offset = SCREENHEIGHT/3
+    y2 = offset+random.randrange(0, int(SCREENHEIGHT -
+                                        GAME_SPRITES['base'].get_height() - 1.2*offset))
+    pipeX = SCREENWIDTH + 10
+    y1 = pipeHight - y2 + offset
+    pipe=[
+        {'x':pipeX, 'y': -y1},
+        {'x':pipeX, 'y': y2} 
+    ]
+    return pipe
 
 
 
@@ -52,7 +65,7 @@ if __name__ == '__main__':
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     pygame.display.set_caption('Flappy Bird Game')
-    GAME_SPRITES['numbers'] =(
+    GAME_SPRITES['numbers'] = (
         pygame.image.load('gallery/sprites/0.png').convert_alpha(),
         pygame.image.load('gallery/sprites/1.png').convert_alpha(),
         pygame.image.load('gallery/sprites/2.png').convert_alpha(),
@@ -65,18 +78,19 @@ if __name__ == '__main__':
         pygame.image.load('gallery/sprites/9.png').convert_alpha(),
     )
 
-    GAME_SPRITES['message'] =pygame.image.load('gallery/sprites/message.png').convert_alpha()
-    GAME_SPRITES['base'] =pygame.image.load('gallery/sprites/base.png').convert_alpha()
-    GAME_SPRITES['pipe'] =(pygame.transform.rotate(pygame.image.load( PIPE).convert_alpha(), 180), 
-    pygame.image.load(PIPE).convert_alpha()
-    )
-    
+    GAME_SPRITES['message'] = pygame.image.load(
+        'gallery/sprites/message.png').convert_alpha()
+    GAME_SPRITES['base'] = pygame.image.load(
+        'gallery/sprites/base.png').convert_alpha()
+    GAME_SPRITES['pipe'] = (pygame.transform.rotate(pygame.image.load(PIPE).convert_alpha(), 180),
+                            pygame.image.load(PIPE).convert_alpha()
+                            )
 
-    GAME_SOUNDS['die']=pygame.mixer.Sound('gallery/audio/die.wav')
-    GAME_SOUNDS['hit']=pygame.mixer.Sound('gallery/audio/hit.wav')
-    GAME_SOUNDS['point']=pygame.mixer.Sound('gallery/audio/point.wav')
-    GAME_SOUNDS['swoosh']=pygame.mixer.Sound('gallery/audio/swoosh.wav')
-    GAME_SOUNDS['wing']=pygame.mixer.Sound('gallery/audio/wing.wav')
+    GAME_SOUNDS['die'] = pygame.mixer.Sound('gallery/audio/die.wav')
+    GAME_SOUNDS['hit'] = pygame.mixer.Sound('gallery/audio/hit.wav')
+    GAME_SOUNDS['point'] = pygame.mixer.Sound('gallery/audio/point.wav')
+    GAME_SOUNDS['swoosh'] = pygame.mixer.Sound('gallery/audio/swoosh.wav')
+    GAME_SOUNDS['wing'] = pygame.mixer.Sound('gallery/audio/wing.wav')
 
     GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert()
     GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
